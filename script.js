9b878037ae31376068fe50f2dadb1220d1ffbdcc
eloisaaaaaaaa <164405275+eloisaaaaaaaa@users.noreply.github.com>
2024-06-20 17:03:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal=document.querySelector(".caixa-pricipal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const caixaResultado=document.querySelector(".texto-resultado");
+
+const perguntas[
+{
+    enunciado:"pergunta1"
+    alternativas:[
+      "alternativa1" 
+      "alternativa2" 
+    ]
+}
+]
